dc88eb4b83cdccf27ecfcdd03b2a1abe4c409c5f
BlackadderAnders <dslitefix@gmail.com>
2017-12-20 15:46:26 +0100

    added more dependencies

---
+++ app/src/main/java/com/blackadder/wefika/CollectToken.java
@@ -0,0 +1,31 @@
+package com.blackadder.wefika;
+
+
+import android.app.Service;
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+import static android.content.ContentValues.TAG;
+
+/**
+ * Created by ezbolan on 2017-12-20.
+ */
+
+public class CollectToken extends FirebaseInstanceIdService {
+
+    public String token;
+
+    @Override
+    public void onTokenRefresh() {
+        // Get updated InstanceID token.
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+        // If you want to send messages to this application instance or
+        // manage this apps subscriptions on the server side, send the
+        // Instance ID token to your app server.
+        token = refreshedToken;
+    }
+}
