f3a2d347991991e77365ee353deca2464fd53de2
qmasven <martin.ni.svensson@gmail.com>
2017-12-20 15:40:21 +0100

    My masterpiece is complete

--- app/src/main/java/com/blackadder/wefika/MainActivity.java
+++ app/src/main/java/com/blackadder/wefika/MainActivity.java
@@ -4,28 +4,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
     Uplink uplink = new Uplink();
+    Date now = new Date();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText test2 = findViewById(R.id.editText2);
+        String time = sdf.format(now);
+        test2.setHint(time);
+
     }
 
     public void doStuff(View view) {
+
         EditText test1 = findViewById(R.id.editText);
         EditText test2 = findViewById(R.id.editText2);
 
         String testText1 = test1.getText().toString();
         String testText2 = test2.getText().toString();
-        test2.setText(testText1 + testText2, TextView.BufferType.NORMAL);
+
+        if (testText2.isEmpty())  {
+            testText2 = sdf.format(now);
+        }
 
         uplink.sendText(testText1 + testText2);
+
+        test1.setText("");
+        test2.setText("");
     }
 
 }
