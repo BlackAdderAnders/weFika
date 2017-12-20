18e1cf154053f297d4c44838eb901aa05dbb8cd5
qmasven <martin.ni.svensson@gmail.com>
2017-12-20 13:42:35 +0100

    New awesome functions to do absolutly nothing

--- app/src/main/java/com/blackadder/wefika/MainActivity.java
+++ app/src/main/java/com/blackadder/wefika/MainActivity.java
@@ -2,6 +2,9 @@ package com.blackadder.wefika;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void doStuff(View view) {
+        EditText test1 = findViewById(R.id.editText);
+        EditText test2 = findViewById(R.id.editText2);
+        String testText = test1.getText().toString();
+        test2.setText(testText, TextView.BufferType.NORMAL);
+    }
+
 }
