90cb5b8c8ea776e06cd9dd5602dfd1d2dfb93605
qmasven <martin.ni.svensson@gmail.com>
2017-12-20 14:15:59 +0100

    Even more shirt-stuff

--- app/src/main/java/com/blackadder/wefika/MainActivity.java
+++ app/src/main/java/com/blackadder/wefika/MainActivity.java
@@ -8,17 +8,24 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    Uplink uplink = new Uplink();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     public void doStuff(View view) {
         EditText test1 = findViewById(R.id.editText);
         EditText test2 = findViewById(R.id.editText2);
-        String testText = test1.getText().toString();
-        test2.setText(testText, TextView.BufferType.NORMAL);
+
+        String testText1 = test1.getText().toString();
+        String testText2 = test2.getText().toString();
+        test2.setText(testText1 + testText2, TextView.BufferType.NORMAL);
+
+        uplink.sendText(testText1 + testText2);
     }
 
 }
